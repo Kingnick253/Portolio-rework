0c3faf4deb33b80b5a03f169a8f9ea56e7ee8260
Kingnick253 <nicholaswebb2016@gmail.com>
2023-01-16 20:49:30 -0800

    Created array to house the inbuttons for the NavBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,26 @@ import React from 'react'
 import {FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+
+  // array for each of the buttons on the navbar
+  const links = [
+    {
+      id:1,
+      link:'About'
+    },
+    {
+      id:2,
+      link:'Projects'
+    },
+    {
+      id:3,
+      link:'Skills'
+    },
+    {
+      id:4,
+      link:'Contact Me'
+    }
+  ]
   return (
     <div className='flex justify-between items-center w-full h-20 text-white  px-4 bg-black  fixed'>
         <div>
@@ -11,8 +31,15 @@ const Navbar = () => {
         </div>
         
         <ul className='flex'>
-            <li className='px-4 cursor-pointer font-medium text-gray-500'>About</li>
-            <li className='px-4'>Projects</li>
+          {/* This will map through the array and auto fill the information */}
+          {links.map(({ id, link }) => (
+              <li key={id}className='px-4 cursor-pointer font-medium text-amber-900 hover:scale-105 duration-200'>
+                {link}
+              </li>
+          ))}
+          
+            
+            
 
         </ul>
     </div>
