e4c888c65b9d9b3ba9c1a1e3ad9c2c3226e3a6d2
Kingnick253 <nicholaswebb2016@gmail.com>
2023-01-22 06:29:08 -0800

    Still working through mobile dropdown menu error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,8 @@ import { FaBars, FaTimes } from "react-icons/fa";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
 
+ 
+  
   // array for each of the buttons on the navbar
   const links = [
     {
@@ -28,7 +30,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 text-white  px-4 bg-black  fixed">
       <div>
-        {/* name in the top right subject to be changed */}
+        {/* name in the top right subject to be changed */},
         <h1 className=" text-5xl font-signature ml-2">Nicholas Webb</h1>
       </div>
 
@@ -46,16 +48,23 @@ const Navbar = () => {
 
       <div
         onClick={() => setNav(!nav)}
-        className="cursor-pointer pr-4 z-10 text-gray-500 md:hidden"
+        className="cursor-pointer pr-4 z-10 text-gray-500"
       >
         {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+            </div>
+        {nav && (
+            <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-FrostOne to-FrostTwo text-gray-500">
+                {links.map(({ id, link }) => (
+                    <li
+                        key ={id}
+                        className="px-4 cursor-pointer capitalize py-6 text-4xl" >
+                        {link}
+                    </li>
+             ))}
+        </ul>
+        )}
       </div>
-      {/* sets up options for the drop down */}
-      <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-FrostOne to-FrostTwo text-gray-500">
-        <li className="px-4 cursor-pointer capitalize py-6 text-4xl">About</li>
-       
-      </ul>
-    </div>
+    
   );
 };
 
