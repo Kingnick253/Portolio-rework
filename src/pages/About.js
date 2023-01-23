f351674c2ff9afd459c58db40cace0599ac47852
Kingnick253 <nicholaswebb2016@gmail.com>
2023-01-23 11:14:33 -0800

    Fixed padding for p tag and title along with padding for profile picture. Started customizing project button

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,14 +6,14 @@ const About = () => {
     <div name="About" className="h-screen w-full bg-gradient-to-b from-FrostOne to-FrostTwo text-white">
         <div className='max-w-sceen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row'>
             <div  className='flex flex-col justify-center h-full'>
-                <img src={ProfilePic} alt="Profile picture of myself with the sun hitting my skin" className= " h-auto w-2/3 mx-auto md:w-full rounded-3xl"/>
+                <img src={ProfilePic} alt="Profile picture of myself with the sun hitting my skin" className= "  h-auto w-96 mx-auto pr-4 pt-4 rounded-3xl md:min-h"/>
             </div>
             <div> 
                 <h2 className='text-4xl sm:text-7xl font-bold'>Software Engineer </h2>
-                <p>I have a year of experience in creating and designing software. I am a recent graduate from University of Washington Coding BootCamp, and have completely falling in love with coding.</p>
+                <p className= " text-gray-500  py-6 max-w-md">I have a year of experience in creating and designing software. I am a recent graduate from University of Washington Coding BootCamp, and have completely falling in love with coding.</p>
 
                 <div>
-                    <button>
+                    <button className="text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r ">
                         Projects 
                         <span>
                             <BsForwardFill />
