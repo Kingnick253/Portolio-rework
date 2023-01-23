d1f05a860232aadf9eec6103db96a76f75a255b9
Kingnick253 <nicholaswebb2016@gmail.com>
2023-01-22 17:10:35 -0800

    finished the base layout for the about section of the page. Will have to resize photo for large screens

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,11 +5,11 @@ const About = () => {
   return (
     <div name="About" className="h-screen w-full bg-gradient-to-b from-FrostOne to-FrostTwo text-white">
         <div className='max-w-sceen-lg mx-auto flex flex-col items-center justify-center h-full px-5 md:flex-row'>
-            <div>
+            <div  className='flex flex-col justify-center h-full'>
                 <img src={ProfilePic} alt="Profile picture of myself with the sun hitting my skin" className= " h-auto w-2/3 mx-auto md:w-full rounded-3xl"/>
             </div>
             <div> 
-                <h2>FULL stack Developer </h2>
+                <h2 className='text-4xl sm:text-7xl font-bold'>Software Engineer </h2>
                 <p>I have a year of experience in creating and designing software. I am a recent graduate from University of Washington Coding BootCamp, and have completely falling in love with coding.</p>
 
                 <div>
