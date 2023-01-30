46479e56093675d106fce810d827614e80ef7954
Kingnick253 <nicholaswebb2016@gmail.com>
2023-01-30 11:25:21 -0800

    Created file to hold of the data  for my Socials. Along with styled the to appeat on the page on the right hand side.

---
+++ src/components/SocialLinks.js
@@ -0,0 +1,51 @@
+import { BsFillPersonLinesFill, BsMailbox } from 'react-icons/bs';
+import{ FaGithub, FaLinkedin,  } from 'react-icons/fa';
+const SocialLinks = [
+    {
+        id: 1,
+        child: (
+            <>
+                LinkedIn <FaLinkedin  size={30}/>
+            </>
+        ),
+        href: 'https://linkedin.com',
+        style: 'rounded-tr-md'
+
+    },
+    {
+        id:2,
+        child: (
+            <>
+                Github <FaGithub size={30}/>
+            </>
+        ),
+        href: 'https://github.com/Kingnick253',
+        
+
+    },
+    {
+        id:3,
+        child: (
+            <>
+                Resume <BsFillPersonLinesFill size={30}/>
+            </>
+        ),
+        href: '',
+        download: true,
+       
+
+    },
+    {
+        id:4,
+        child: (
+            <>
+                Email <BsMailbox  size={30}/>
+            </>
+        ),
+        href: 'mailto:nicholaswebb2016@gmail.com',
+       style:'rounded-br-md',
+
+    },
+]
+
+export default SocialLinks;
