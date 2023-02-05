873df959ea312d11d860608a343784a510dd4249
Kingnick253 <nicholaswebb2016@gmail.com>
2023-02-05 10:45:44 -0800

    finished mapping array for Project data. Will continue customizing

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PaperBoy from "../images/paperboy.png";
+import ProjectsData from './ProjectsData';
 const Projects = () => {
   return (
         <div name ="Portfolio" className='bg-gradient-to-b from-FrostOne to-FrostTwo text-white md:h-screen'>
@@ -9,18 +10,22 @@ const Projects = () => {
                     <p className='text-4xl font-bold inline border-b-4'>Portfolio</p>
                     <p></p>
                 </div>
-
-                <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8
+                <div  className='grid sm:grid-cols-2 md:grid-cols-3 gap-8
                 px-12 sm:px-0'>
-                    <div className=' shadow-md shadow-gray-600 roudned-lg'>
-                        <img src={PaperBoy} alt='' className='rounded-md duration-200 hover:scale-105'/>    
+                {
+                    ProjectsData.map(({id, title, image, github, link  }) => (
+
+                    <div key={id} className=' shadow-md shadow-gray-600 roudned-lg'>
+                        <img src={image} alt='' className='rounded-md duration-200 hover:scale-105'/>    
                         <div className='flex items-center justify-center'>
-                            <button className=''>Application</button>
-                            <button>Github </button>
+                            <button  className='w-1/2 px-6 py-3 m-4 duration-200 hover: scale-105'>Application</button >
+                            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover: scale-105'>Github </button>
                         </div>
                     </div>
 
-                </div>
+                ))}
+            </div>
+                
 
 
             </div>
--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -1,21 +1,36 @@
-import PaperBoy from "../images/paperboy.png"
-
+import PaperBoy from "../images/paperboy.png";
+import NoteTaker from "../images/notetaker.png";
+import PassGen from "../images/PasswordGen.png";
+import MusicM from "../images/MusicMatch.png";
 const ProjectsData = [
     {
-        id:1,
+        id: 1,
         title:"Paper Boy",
         image:PaperBoy,
-        Github: "https://github.com/paullsshin/Paper-Boy",
-        Link: "https://paullsshin.github.io/Paper-Boy/"
+        github: "https://github.com/paullsshin/Paper-Boy",
+        link: "https://paullsshin.github.io/Paper-Boy/"
 
     },
     {
-
+        id: 2,
+        title:"TakeMyNotes",
+        image:NoteTaker,
+        github: "https://github.com/paullsshin/Paper-Boy",
+        link: "https://paullsshin.github.io/Paper-Boy/"
     },
     {
-
+        id: 3,
+        title:"Password Generator",
+        image:PassGen,
+        github: "https://github.com/paullsshin/Paper-Boy",
+        link: "https://paullsshin.github.io/Paper-Boy/"
     },
     {
-
+        id: 4,
+        title:"Music Match",
+        image: MusicM,
+        github: "https://github.com/paullsshin/Paper-Boy",
+        link: "https://paullsshin.github.io/Paper-Boy/"
     },
 ]
+export default ProjectsData;
