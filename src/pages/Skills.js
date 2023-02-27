1d084aa6062492ba888cbe4f3a71f3e92e8167fe
Kingnick253 <nicholaswebb2016@gmail.com>
2023-02-27 11:32:30 -0800

    finished the base edits for skills page. About to start creating a data file for the rest of the icons

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -12,9 +12,20 @@ import Tailwind from '../icons/tailwind-css-1.svg';
 
 const Skills = () => {
   return (
-    <div>
+    <div id="skills">
+        <div className='bg-gradient-to-b from-FrostTwo to-FrostOne w-full h-screen'>
+            <div className='flex max-w-screen-lg mx-auto p-4 flex-col justify-center w-full h-full text-white'>
+                <p className='text-4xl font-bold border-b-4 p-2 inline'>Technologies</p>
+                <p>I have experience with</p>
 
-
+            <div className=' w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-9 px-12 sm:px-0'>
+              <div className=' shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
+                <img  src={Css} alt="" className=' w-20 mx-auto'/>
+                <p className='mt-3'>CSS</p>
+              </div>
+            </div>
+            </div>
+        </div>
     </div>
   )
 }
