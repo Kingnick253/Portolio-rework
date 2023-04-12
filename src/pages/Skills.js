f041fb7964d3890f2657301ad19bd6ec5ba36b17
Kingnick253 <nicholaswebb2016@gmail.com>
2023-04-11 20:41:16 -0700

    fixed the sizing for the icons

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -20,7 +20,7 @@ const Skills = () => {
                 key={id}
                 className={ ` shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}
               >
-                <img src={icon} alt="" className=" w-20 mx-auto" />
+                <img src={icon} alt="" className="  object-scale-down h-20 w-20 w-20 mx-auto" />
                 <p className="mt-3">{title}</p>
               </div>
             ))}
